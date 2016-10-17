bacf658a9669183b4799fc2f028131345385cf74
Alik Khilazhev <alikhil@mail.ru>
2016-10-17 20:26:48 +0300

    Adds tests dir.

---
+++ tests/compress-tests.js
@@ -0,0 +1,18 @@
+var chai = require("chai");
+var expect = chai.expect; // we are using the "expect" style of Chai
+var compresser = require("./../compress");
+
+describe("Compression", function() {
+  it("compress() should return string with less length than initial\'s", function() {
+    var str = "ababab123456789";
+    var compressed = compresser.compress(str);
+    expect(compressed.length).to.be.below(str.length);
+  });
+
+  it("decompress() should retutn initial string before compression", function() {
+  	var str = "hello world";
+  	var compressed = compresser.compress(str);
+  	expect(compresser.decompress(compressed)).to.equal(str);
+  });
+});
+
