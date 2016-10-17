5121c5bed583794e599e7b09721f5b7b88a5c71c
Alik Khilazhev <alikhil@mail.ru>
2016-10-17 21:57:37 +0300

    Adds header and tests for AES.

---
+++ tests/encrypt-tests.js
@@ -0,0 +1,20 @@
+var chai = require("chai");
+var expect = chai.expect; 
+var crypter = require("./../encrypt.js");
+
+describe("AESEnctyption", function() {
+  it("encryptAES(str, key) should return string encrypted by AES", function() {
+    var str = "ababab123456789";
+    var key = "145314145314ASDQWEFRGS1453141453"; // 32 key number
+    var encrypted = crypter.encryptAES(str, key);
+    expect(encrypted).to.not.equal(str);
+  });
+
+  it("decryptAES(str, key) should return decrypted string", function() {
+  	var str = "hello world";
+    var key = "145314145314ASDQWEFRGS1453141453"; // 32 key number
+  	var encrypted = crypter.encryptAES(str, key);
+  	expect(crypter.decryptAES(encrypted, key)).to.equal(str);
+  });
+});
+
