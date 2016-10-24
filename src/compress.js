65d30ad4f3841e7041d2cdf17fbc33c1b00cf600
Nothing-personal <kev100500@gmail.com>
2016-10-24 20:03:36 +0300

    Adds Collections library into project

--- src/compress.js
+++ src/compress.js
@@ -1,7 +1,9 @@
 var LZString = require("lz-string");
+var Set = require("collections/set");
 
 function compress(jsonData) {
-	return LZString.compress(jsonData);
+	//return LZString.compress(jsonData);
+	var dictionary = new Set();
 }
 
 function decompress(compressed) {
