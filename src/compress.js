4a090bd8bd4eaf952e8d87f5a9afd787704b63db
Nothing-personal <kev100500@gmail.com>
2016-10-24 23:48:28 +0300

    Adds function to count symbols occurrences and test for this function.

--- src/compress.js
+++ src/compress.js
@@ -1,14 +1,34 @@
 var LZString = require("lz-string");
-var Set = require("collections/set");
+var HashMap = require("hashmap");
 
 function compress(jsonData) {
-	//return LZString.compress(jsonData);
-	var dictionary = new Set();
+	return LZString.compress(jsonData);
 }
 
 function decompress(compressed) {
 	return LZString.decompress(compressed);
 }
 
+/**
+ * Function that counts number of symbols occurrences in string and returns
+ * @param input string that is expected to be JSON (but not a mandatory)
+ * @returns {*} a map < symbol, count >
+ */
+function countOccurrences(input) {
+	var occurrencesMap = new HashMap();
+	for (var i = 0; i < input.length; i++) {
+		var currentChar = input.charAt(i);
+		if (occurrencesMap.has(currentChar)) {
+			var charCount = occurrencesMap.get(currentChar);
+			charCount++;
+			occurrencesMap.set(currentChar, charCount);
+		} else {
+			occurrencesMap.set(currentChar, 1);
+		}
+	}
+	return occurrencesMap;
+}
+
 exports.compress = compress;
 exports.decompress = decompress;
+exports.countOccurences = countOccurrences;
--- test/compress-tests.js
+++ test/compress-tests.js
@@ -11,17 +11,25 @@ describe("Compression", function () {
 
     it("decompress() should retutn initial string before compression", function () {
         var str = JSON.stringify({ id : "780710", folderID : "42024", displayOrder : 3});
-        
         var compressed = compresser.compress(str);
         expect(compresser.decompress(compressed)).to.equal(str);
     });
 
     it("compress() should compress JSON string and then decompress and return the initial string: ", function () {
-        
         var str = JSON.stringify({ id : "780710", folderID : "42024", displayOrder : 2});
         var compressed = compresser.compress(str);
         var decompressed = compresser.decompress(compressed);
         expect(decompressed).to.equal(str);
+    });
+
+    it("countOccurrences() counts symbols occurrences in a " +
+        "string input and then returns a map \< symbol, count \>: ", function () {
+        var str = "test.";
+        var resultMap = compresser.countOccurences(str);
+        expect(resultMap.get("t")).to.equal(2);
+        expect(resultMap.get("e")).to.equal(1);
+        expect(resultMap.get("s")).to.equal(1);
+        expect(resultMap.get(".")).to.equal(1);
     });
 });
 
