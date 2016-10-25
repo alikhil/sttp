b534c6183d6a6e54eec8fc811af3b2920b8ccacc
Your Name <sergey_umnov@mail.ru>
2016-10-25 13:01:30 +0300

    Implements xorWord function

--- src/aes.js
+++ src/aes.js
@@ -92,7 +92,13 @@ function subBytes(state) {
 }
 
 function xorWords(word1, word2) {
-
+	var newWord = [
+		word1[0] ^ word2[0],
+		word1[1] ^ word2[1],
+		word1[2] ^ word2[2],
+		word1[3] ^ word2[3],
+	];
+	return newWord;
 }
 
 function subWord(word) {
