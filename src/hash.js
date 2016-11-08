acf9fcb350b76136a10f2aea67ebe58d594b68a7
Alik Khilazhev <alikhil@mail.ru>
2016-11-08 10:42:38 +0300

    Adds hash function implementation.

--- src/hash.js
+++ src/hash.js
@@ -1,6 +1,14 @@
+// The hashing like in java String.getHashCode()
 
 function hash(str) {
-	return "#";
+	var hash = 0, i, chr, len;
+	if (str.length === 0) return hash;
+	for (i = 0, len = str.length; i < len; i++) {
+		chr = str.charCodeAt(i);
+		hash = ((hash << 5) - hash) + chr;
+		hash |= 0; // Convert to 32bit integer
+	}
+	return hash;
 }
 
 
