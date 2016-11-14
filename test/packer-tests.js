9a283ce3430d13f59ca148c8615a4cd71a441254
Alik Khilazhev <alikhil@mail.ru>
2016-11-14 23:04:27 +0300

    Updates test/packer-tests.js.
    * Adds tests for DataPacker.
    * Adds tests for AuthDataPacker.

--- test/packer-tests.js
+++ test/packer-tests.js
@@ -1,7 +1,7 @@
 var chai = require("chai");
 var expect = chai.expect; 
 var assert = chai.assert;
-
+var hasher = require('../src/hash.js');
 var rsa = require("./../src/rsa.js");
 var packer = require("./../src/packer.js");
 var DataPacker = packer.DataPacker;
@@ -29,6 +29,14 @@ describe("Packer", function(){
 				var packedData = dataPacker.pack({key : "value", array: [1, 2]});
 
 				assert.match(packedData, /^([0-9A-Za-z])+$/g, "packed data is not in base64");
+			});
+
+			it("Should return packet containing 'data' and 'hash' fields.", function() {
+				var dataPacker = new DataPacker(AES_KEY);
+				var packet = dataPacker.pack({key : "value", array: [1, 2]});
+
+				expect(packet).to.have.property("data");
+				expect(packet).to.have.property("hash", hasher.hash(packet.data));
 			});
 		});
 
@@ -62,6 +70,28 @@ describe("Packer", function(){
 				var authKeyPacker = new AuthKeyPacker(rsaKey.privateKey, true);
 				expect(authKeyPacker.canEncrypt()).to.be.true;
 				expect(authKeyPacker.canDecrypt()).to.be.true;
+			});
+		});
+
+		describe("pack(authKey)", function() {
+			it("Should encrypt with public RSA key and return it as packet containing 'data' and 'hash' fields.", function() {
+				var rsaKey = rsa.generateRSAKeys();
+				var authKeyPacker = new AuthKeyPacker(rsaKey.privateKey, true);
+				var aesKey = "0000111122223333";
+				var packet = authKeyPacker.pack(aesKey);
+				expect(packet).to.have.property("data");
+				expect(packet).to.have.property("hash", hasher.hash(packet.data));
+			});
+		});
+
+		describe("unpack(packet)", function() {
+			it("Should decrypt packet created by 'pack()' and return initial data.", function() {
+				var rsaKey = rsa.generateRSAKeys();
+				var authKeyPacker = new AuthKeyPacker(rsaKey.privateKey, true);
+				var aesKey = "0000111122223333";
+				var packet = authKeyPacker.pack(aesKey);
+				var unpacked = authKeyPacker.unpack(packet);
+				expect(unpacked).to.be.equal(aesKey);
 			});
 		});
 
