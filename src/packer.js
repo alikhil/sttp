4c44ab12998d7644c807e4bd9f9fb152c3a618c5
Alik Khilazhev <alikhil@mail.ru>
2016-11-18 00:20:13 +0300

    Updates packer.js
    * Implements AuthDataPacker.unpack,

--- src/packer.js
+++ src/packer.js
@@ -1,5 +1,6 @@
 var compresser = require("../src/compress.js");
 var aesCrypter = require("../src/aes.js");
+var rsaCrypter = require("../src/rsa.js");
 var hasher = require("../src/hash.js");
 var util = require("../src/util.js");
 var base64 = require("../src/Base64.js").Base64(); 
@@ -36,15 +37,15 @@ var DataPacker = function(aesKey) {
 	return this;
 };
 
-var AuthKeyPacker = function(key, private) {
+var AuthKeyPacker = function(key, isPrivate=false) {
 	
-	this.private = private || false;
+	this.isPrivate = isPrivate;
 
 	function hasKey(obj, key) {
 		return obj.hasOwnProperty(key);
 	}
 
-	this.hasPrivateKey = private;
+	this.hasPrivateKey = isPrivate;
 	this.key = key;
 
 
@@ -57,6 +58,21 @@ var AuthKeyPacker = function(key, private) {
 		return this.hasPrivateKey && this.canEncrypt()
 			&& hasKey(key, "P") && hasKey(key, "Q")
 			&& hasKey(key, "D") && hasKey(key, "F");
+	};
+
+	this.pack = function(data) {
+		if (typeof data !== "string") 
+			data = JSON.stringify(data);
+		var crypted = rsaCrypter.encryptRSA(data, key);
+		var result = { data: crypted, hash: hasher.hash(crypted) };
+		return base64.encode(JSON.stringify(result));
+	};
+
+	this.unpack = function(rawData) {
+		var decoded = base64.decode(rawData);
+		var result = JSON.parse(decoded);
+		var decrypted = rsaCrypter.decryptRSA(result.data, key);
+		return decrypted;
 	};
 };
 
